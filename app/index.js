181f36fbba08b3d1d0c75c0b267c3aaaec1047e9
BryanJBryce <bryan.bryce@gmail.com>
2018-05-01 23:04:17 -0700

    Hello World!

--- app/index.js
+++ app/index.js
@@ -1,7 +1,18 @@
 var React = require('react');
 var ReacDOM = require('react-dom');
-require('/.index.css');
+require('./index.css');
 
 class App extends React.Component {
-
+	render() {
+		return (
+			<div>
+        Hello Bryan!
+      </div>
+		)
+	}
 }
+
+ReacDOM.render(
+  <App />,
+  document.getElementById('app')
+);
